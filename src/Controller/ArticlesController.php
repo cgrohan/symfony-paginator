ea84027dfc64195492afa7d4032b1f4adfdf2f2f
Clément GROHAN <clement.grohan@mes-services.me>
2024-04-10 16:31:44 +0200

    mise en place de la logique de pagination et template twig

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,15 +4,21 @@ namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(ArticlesRepository $repoArticle)
+    public function index(Request $request, ArticlesRepository $repoArticle)
     {       
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $repoArticle->getArticlePaginator($offset);
+
         return $this->render('home/index.html.twig',[
-            'articles' => $repoArticle->findAll()
+            'articles' => $paginator,
+            'previous' => $offset - $repoArticle::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + $repoArticle::PAGINATOR_PER_PAGE),
         ]);
     }
 }
